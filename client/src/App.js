6d6673e5ddff9f9e41b240104afe8dd6fb78f039
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-06-12 19:01:42 +0200

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
+import AppNavbar from "./components/AppNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <AppNavbar />
     </div>
   );
 }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+
+const AppNavbar = () => {
+  return (
+    <div>
+      <Navbar color="dark" dark>
+        <Container>
+          <NavbarBrand href="/">Download v2</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="add">Add/Upload</NavLink>
+            </NavItem>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default AppNavbar;
