a61a2cdf1095335028c8ed75c1fabf36178f1a84
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-06-12 19:11:17 +0200

    Navbar link & cleanup

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -7,9 +7,12 @@ const AppNavbar = () => {
       <Navbar color="dark" dark>
         <Container>
           <NavbarBrand href="/">Download v2</NavbarBrand>
-          <Nav className="ml-auto" navbar>
+          <Nav className="ml-auto flex-row" navbar>
             <NavItem>
               <NavLink href="add">Add/Upload</NavLink>
+            </NavItem>
+            <NavItem style={{ marginLeft: "16px" }}>
+              <NavLink href="https://github.com/hynekfisera/download-new">GitHub</NavLink>
             </NavItem>
           </Nav>
         </Container>
--- routes/api/items.js
+++ routes/api/items.js
@@ -3,18 +3,12 @@ const router = express.Router();
 
 const Item = require("../../models/Item");
 
-// @route   GET api/items
-// @desc    Get items
-// @access  Public
 router.get("/", (req, res) => {
   Item.find()
     .sort({ date: -1 })
     .then((items) => res.json(items));
 });
 
-// @route   POST api/items
-// @desc    Create an item
-// @access  Public
 router.post("/", (req, res) => {
   const newItem = new Item({
     name: req.body.name,
@@ -26,9 +20,6 @@ router.post("/", (req, res) => {
   newItem.save().then((item) => res.json(item));
 });
 
-// @route   DELETE api/items/:id
-// @desc    Delete an item
-// @access  Public
 router.delete("/:id", (req, res) => {
   Item.findById(req.params.id)
     .then((item) => item.remove().then(() => res.json({ success: true })))
